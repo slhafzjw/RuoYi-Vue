2919379a5f3690fca18a4cc384d008d5d683adfb
slhafzjw <slhafzjw@slhaf.work>
2025-12-16 15:29:24 +0800

    docs: clarify exception resolution

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
@@ -77,7 +77,7 @@ public class SysLoginService
             ASK AuthenticationManager 认证流程？
             ANSWER
                 借助调试可以看到，authenticationManager.getClass() 看到的类型实际为 ProviderManager
-                而在 ProviderManaer 中可以看到存在一个 DaoAuthenticationProvider
+                而在 ProviderManager 中可以看到存在一个 DaoAuthenticationProvider
                 翻阅源码(至 AbstractDaoAuthenticationConfigurer 处)可以得知，DaoAuthenticationProvider 为 UserDetails 方式下的默认 Provider
                 Provider 在构造时通过构造方法注入 UserDetailsService
                 DaoAuthenticationProvider 继承自 AbstractUserDetailsAuthenticationProvider，实现模板方法
@@ -103,7 +103,7 @@ public class SysLoginService
             AuthenticationContextHolder.clearContext();
         }
         AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_SUCCESS, MessageUtils.message("user.login.success")));
-        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
+        LoginUser loginUser  = (LoginUser) authentication.getPrincipal();
         recordLoginInfo(loginUser.getUserId());
         // 生成token
         /*
@@ -148,7 +148,21 @@ public class SysLoginService
             {
                 //TODO 异步任务如何执行，它的调度方式有哪些？
                 AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.jcaptcha.expire")));
-                //TODO 异常处理机制与异常体系？
+                /*
+                ASK 异常处理体系
+                ANSWER
+                    若依的异常体系确实要丰富一些
+                    以此处的 CaptchaExpireException 为例
+                    整个继承链为: CaptchaExpireException  -> UserException -> BaseException -> RuntimeException
+                    异常构造过程中，会将对应的异常码逐级向上传递，最终存储在 BaseException 中定义的 code 属性中
+                    最终的 BaseException 异常又重写了 getMessage 方法，方法内部调用 MessageUtils 获取该异常码对应的 i18n 错误信息
+                    而 GlobalExceptionHandler 中定义的全局异常处理逻辑，则只负责对消息进行必要的清洗并通过 log.error() 打印异常信息并记录
+
+                    对于 MessageUtils 中的国际化处理，该工具类内部的注释提到会提到委托给 Spring MessageSource
+                    MessageSource 作为 Spring 提供的‘国际化消息查找抽象接口’，可根据 Locale 扫描对应的 messages.properties 并获取文本
+
+                    这种方式确实是比‘将异常信息写死在异常抛出位置’要灵活、统一多了
+                 */
                 throw new CaptchaExpireException();
             }
             redisCache.deleteObject(verifyKey);
